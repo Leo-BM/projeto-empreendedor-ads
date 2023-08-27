75e55e43f5c104f37a7fd4e0566c01349e99b1c7
Leo-BM <leo_bmaria@yahoo.com>
2023-08-26 23:00:48 -0300

    Add link para registro a partir da página de login

--- src/Views/LoginPage.vue
+++ src/Views/LoginPage.vue
@@ -46,8 +46,11 @@
         Sua senha está incorreta.
       </p>
     </div>
+    <div class="text-center">
+      <RouterLink class="text-black text-base" :to="{name: 'registrationPage'}">Cadastre-se aqui</RouterLink>
+    </div>
 
-    <div class="flex items-center justify-between">
+    <div class="flex items-center justify-end">
       <button
         @click="submitLogin"
         class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline disabled:bg-slate-400"
