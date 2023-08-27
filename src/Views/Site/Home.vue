224f9e59cc8de4db5499a46bf20c43c9c625154f
Leo-BM <leo_bmaria@yahoo.com>
2023-08-26 22:01:16 -0300

    Criado componente de Home

---
+++ src/Views/Site/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1 class="text-3xl text-center">
+      Boas vindas ao SISC
+      <span class="text-xs">Sistema integrado de saúde Caxias </span>
+    </h1>
+    <div>
+      <RouterLink :to="{ name: 'loginPage' }">Login</RouterLink>
+    </div>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped></style>
