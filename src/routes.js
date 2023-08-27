cef445e0a7f8530a3c69b37739783cc6cd4f48e2
Leo-BM <leo_bmaria@yahoo.com>
2023-08-26 22:25:05 -0300

    Add rota da sucesso no login

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,12 @@
 import { createRouter, createWebHistory } from "vue-router";
 import LoginPage from "./Views/LoginPage.vue";
 import HomeView from "./Views/Site/Home.vue";
+import DashboardView from "./Views/Admin/Dashboard.vue";
 
 const routes = [
-  { path: "/", component: HomeView, name:"homePage" },
+  { path: "/", component: HomeView, name: "homePage" },
   { path: "/acesso", component: LoginPage, name: "loginPage" },
+  { path: "/painel", component: DashboardView, name: "DashboardView" },
 ];
 
 const router = createRouter({
