e4ad3a39ec8d4c514c9da6f0033db28cbbe5755a
Leo-BM <leo_bmaria@yahoo.com>
2023-09-09 15:11:32 -0300

    Add layout Default

---
+++ src/Layouts/Default.vue
@@ -0,0 +1,54 @@
+<template>
+  <div>
+    <h1 class="text-3xl">SISC</h1>
+    <header class="mb-4">
+      <nav>
+        <ul>
+          <li>
+            <a href="javascript:;">Início</a>
+          </li>
+          <li>
+            <a href="javascript:;">Exames</a>
+          </li>
+          <li>
+            <a href="javascript:;">Consultas</a>
+          </li>
+          <li>
+            <a href="javascript:;">Vacinas</a>
+          </li>
+          <li>
+            <a href="javascript:;">Aval.</a>
+          </li>
+          <li>
+            <a href="javascript:;">Help</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+    <slot></slot>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped>
+ul {
+  @apply flex justify-between;
+}
+li {
+  @apply block flex-1 text-center;
+}
+
+ul li:not(:first-child) a::after {
+  position: absolute;
+  top: 0;
+  display: block;
+  content: "";
+  height: 100%;
+  border-right: 2px solid red;
+}
+
+ul li a {
+  @apply block relative;
+}
+</style>
