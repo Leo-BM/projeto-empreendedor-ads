ca81de1b620a7d5cf00fba50eaacdf842f8310a0
Leo-BM <leo_bmaria@yahoo.com>
2023-09-09 17:06:42 -0300

    Melhorias do componente Select2

--- src/Components/Select2.vue
+++ src/Components/Select2.vue
@@ -1,9 +1,11 @@
 <template>
   <div>
-    <select v-if="options.length" @change="$emit('update:modelValue', this)">
+    <select v-if="options.length" @change="updateValue">
       <option
         v-for="option in options"
         :key="option.id"
+        :selected="option.selected == true"
+        :disabled="option.disabled == true"
         :value="option.id"
       >
         {{ option.label }}
@@ -14,10 +16,18 @@
 </template>
 
 <script setup>
-import { ref, onMounted, defineProps } from "vue";
+import { ref, onMounted, defineProps, defineEmits } from "vue";
 import api from "../Utils/api";
 
-const props = defineProps({ url: String, indexLabel: String });
+const props = defineProps({
+  url: String,
+  indexLabel: String,
+  indexId: {
+    type: String,
+    default: "id",
+  },
+});
+const emits = defineEmits(["update:modelValue"]);
 const options = ref([]);
 const selectedOption = ref(null);
 const error = ref(null);
@@ -27,15 +37,21 @@ const fetchData = async () => {
     const response = await api.get(props.url);
     let dados = response.data.dados.map((i) => ({
       label: i[props.indexLabel],
-      id: i.id,
+      id: i[props.indexId],
     }));
-    console.log(dados);
-    options.value = [{label: "Selecione", id: null}, ...dados ];
+    options.value = [
+      { label: "Selecione", disabled: true, selected: true, id: null },
+      ...dados,
+    ];
   } catch (err) {
     console.log(err);
     error.value = "Erro ao buscar os dados";
   }
 };
+
+function updateValue($event) {
+  emits("update:modelValue", $event.target.value);
+}
 
 onMounted(fetchData);
 </script>
