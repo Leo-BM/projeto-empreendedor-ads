2ebb9bf443703092f5d9e214280d6bffa6ef1f90
Leo-BM <leo_bmaria@yahoo.com>
2023-09-09 15:11:59 -0300

    Add layout em Dashboard

--- src/Views/Admin/Dashboard.vue
+++ src/Views/Admin/Dashboard.vue
@@ -1,16 +1,16 @@
 <template>
-  <div>
-    <h1>SISC</h1>
+  <Default>
     <div class="flex space-x-4 justify-center items-center flex-wrap h-screen">
       <RouterLink class="btn-primary !p-5" :to="{ name: 'AgendarConsulta' }"
       >Agendar consulta</RouterLink
       >
       <!-- <RouterLink class="btn" :to="{ name: AgendarExameView }">Agendar exame</RouterLink> -->
     </div>
-  </div>
+  </Default>
 </template>
 
 <script setup>
+import Default from '@/Layouts/Default.vue';
 </script>
 
 <style scoped>
