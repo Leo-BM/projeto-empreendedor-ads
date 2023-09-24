2a97b92ad9a84712c29ceffdbca05cd5bb7b8d69
Leo-BM <leo_bmaria@yahoo.com>
2023-09-24 16:27:34 -0300

    Add Modal com os dados de uma consulta

--- src/Views/Admin/ConsultasAgendadas.vue
+++ src/Views/Admin/ConsultasAgendadas.vue
@@ -17,30 +17,61 @@
           </p>
           <button
             class="text-right bg-white text-slate-600 px-2 py-1 rounded-md float-right mt-4"
+            @click="acessarConsulta(agenda.id_agenda)"
           >
-            Entrar na consulta (Consulta{{ agenda.id_agenda }})
+            Entrar na consulta
           </button>
         </li>
       </ul>
     </div>
+    <SimpleModal v-model="iframeConsulta">
+      <div class="bg-red-600 w-full">
+        <iframe
+          :src="`https://navve.studio/alpha/index.html?room=ConsultaRemota${idAgenda}&label=${user.email}&remote&screensharehide&effects&sl&hidetranslate&turn=navve;M28shauetSdf;turn:turn.sub.navve.studio:443&cbr&vd`"
+          allow="autoplay;camera;microphone;fullscreen;picture-in-picture;display-capture;midi;geolocation;"
+          frameborder="0"
+          ref="iframeRef"
+          class="w-full h-screen"
+        ></iframe>
+      </div>
+    </SimpleModal>
   </Default>
 </template>
 
 <script setup>
-import { onMounted, ref } from "vue";
+import { onMounted, onBeforeUnmount, ref } from "vue";
 import Default from "@/Layouts/Default.vue";
+import SimpleModal from "../../Components/SimpleModal.vue";
 import api from "@/Utils/api";
 import { format } from "date-fns";
 
 const agendas = ref([]);
 
+const iframeConsulta = ref(false);
+
+const idAgenda = ref(0);
+
 const user = ref(JSON.parse(localStorage.getItem("usuario")));
 
 onMounted(async () => {
   const response = await api.get(`/schedule/appointments/6/${user.value.id}/1`);
-
   agendas.value = response.data.dados;
+
+  window.addEventListener("hangupCall", callEnded);
 });
+
+onBeforeUnmount(() => {
+  window.removeEventListener("hangupCall", callEnded);
+});
+
+function acessarConsulta(idAgenda) {
+  iframeConsulta.value = true;
+  idAgenda.value = idAgenda;
+}
+
+function callEnded() {
+  alert("Chamada encerrada");
+}
 </script>
 
 <style scoped>
