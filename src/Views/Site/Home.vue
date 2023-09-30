14831302b781d8f8d60e56ca67d4b096df670573
Leo-BM <leo_bmaria@yahoo.com>
2023-09-30 11:36:52 -0300

    Reparado tela de Home

--- src/Views/Site/Home.vue
+++ src/Views/Site/Home.vue
@@ -1,15 +1,28 @@
 <template>
-  <div>
-    <h1 class="text-3xl text-center">
-      Boas vindas ao SISC
-      <span class="text-xs">Sistema integrado de saúde Caxias </span>
-    </h1>
-    <div>
-      <RouterLink :to="{ name: 'loginPage' }">Login</RouterLink>
+  <div class="h-screen grid place-items-center">
+    <div class="space-y-10">
+      <div class="text-center">
+        <h1 class="text-5xl underline font-bold mb-4 text-slate-600">SISC</h1>
+        <h2 class="text-slate-600">Sistema integrado de saúde Caxias do Sul</h2>
+      </div>
+      <div class="text-center space-x-5">
+        <RouterLink class="btn-primary acessos" :to="{ name: 'loginPage' }"
+          >Login</RouterLink
+        >
+        <RouterLink
+          class="btn-primary acessos"
+          :to="{ name: 'registrationPage' }"
+          >Inscreva-se</RouterLink
+        >
+      </div>
     </div>
   </div>
 </template>
 
 <script setup></script>
 
-<style scoped></style>
+<style scoped>
+.acessos {
+  @apply inline-block w-32 text-center;
+}
+</style>
