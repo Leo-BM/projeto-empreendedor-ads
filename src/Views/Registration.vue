78855595f7f6a26b18987354ac30d56ba98ea6f9
Leo-BM <leo_bmaria@yahoo.com>
2023-09-30 13:37:11 -0300

    Reparado tela de Registro

--- src/Views/Registration.vue
+++ src/Views/Registration.vue
@@ -1,88 +1,93 @@
 <template>
-  <div class="bg-white p-8 rounded-lg shadow-md w-96 mx-auto">
-    <div class="text-center">
-      <MainLogo />
-    </div>
-    <!-- Mensagem de erro geral -->
-    <div
-      class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative mb-4 invisible"
-      :class="{ '!visible': registrationFailMessage.length != 1 }"
-      id="generalError"
-    >
-      <span class="block sm:inline">{{ registrationFailMessage }}</span>
-    </div>
-
-    <!-- Campo de nome completo -->
-    <div class="mb-4">
-      <label
-        class="block text-slate-600 text-sm font-bold mb-2"
-        for="full_name"
+  <div class="h-screen grid place-items-center">
+    <div class="bg-white p-8 rounded-lg shadow-md w-96 mx-auto">
+      <div class="text-center">
+        <MainLogo />
+      </div>
+      <!-- Mensagem de erro geral -->
+      <div
+        class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative mb-4 invisible"
+        :class="{ '!visible': registrationFailMessage.length != 1 }"
+        id="generalError"
       >
-        Nome completo
-      </label>
-      <input
-        class="shadow appearance-none border-2 rounded border-slate-600 w-full py-2 px-3 text-slate-600 leading-tight focus:outline-none focus:shadow-outline"
-        id="full_name"
-        v-model="registrationForm.full_name"
-        type="text"
-        placeholder="Nome completo"
-      />
-    </div>
+        <span class="block sm:inline">{{ registrationFailMessage }}</span>
+      </div>
 
-    <!-- Campo de data de nascimento -->
-    <div class="mb-4">
-      <label
-        class="block text-slate-600 text-sm font-bold mb-2"
-        for="birth_date"
-      >
-        Data de nascimento
-      </label>
-      <input
-        class="shadow appearance-none border-2 rounded border-slate-600 w-full py-2 px-3 text-slate-600 leading-tight focus:outline-none focus:shadow-outline"
-        id="birth_date"
-        v-model="registrationForm.birth_date"
-        type="date"
-        placeholder="Data de nascimento"
-      />
-    </div>
+      <!-- Campo de nome completo -->
+      <div class="mb-4">
+        <label
+          class="block text-slate-600 text-sm font-bold mb-2"
+          for="full_name"
+        >
+          Nome completo
+        </label>
+        <input
+          class="shadow appearance-none border-2 rounded border-slate-600 w-full py-2 px-3 text-slate-600 leading-tight focus:outline-none focus:shadow-outline"
+          id="full_name"
+          v-model="registrationForm.full_name"
+          type="text"
+          placeholder="Nome completo"
+        />
+      </div>
 
-    <!-- Campo de e-mail -->
-    <div class="mb-4">
-      <label class="block text-slate-600 text-sm font-bold mb-2" for="email">
-        Email
-      </label>
-      <input
-        class="shadow appearance-none border-2 rounded border-slate-600 w-full py-2 px-3 text-slate-600 leading-tight focus:outline-none focus:shadow-outline"
-        id="email"
-        v-model="registrationForm.email"
-        type="email"
-        placeholder="Email"
-      />
-    </div>
+      <!-- Campo de data de nascimento -->
+      <div class="mb-4">
+        <label
+          class="block text-slate-600 text-sm font-bold mb-2"
+          for="birth_date"
+        >
+          Data de nascimento
+        </label>
+        <input
+          class="shadow appearance-none border-2 rounded border-slate-600 w-full py-2 px-3 text-slate-600 leading-tight focus:outline-none focus:shadow-outline"
+          id="birth_date"
+          v-model="registrationForm.birth_date"
+          type="date"
+          placeholder="Data de nascimento"
+        />
+      </div>
 
-    <!-- Campo de senha -->
-    <div class="mb-6">
-      <label class="block text-slate-600 text-sm font-bold mb-2" for="password">
-        Senha
-      </label>
-      <input
-        class="shadow appearance-none border-2 rounded border-slate-600 w-full py-2 px-3 text-slate-600 mb-3 leading-tight focus:outline-none focus:shadow-outline"
-        id="password"
-        type="password"
-        v-model="registrationForm.password"
-        placeholder="******************"
-      />
-    </div>
+      <!-- Campo de e-mail -->
+      <div class="mb-4">
+        <label class="block text-slate-600 text-sm font-bold mb-2" for="email">
+          Email
+        </label>
+        <input
+          class="shadow appearance-none border-2 rounded border-slate-600 w-full py-2 px-3 text-slate-600 leading-tight focus:outline-none focus:shadow-outline"
+          id="email"
+          v-model="registrationForm.email"
+          type="email"
+          placeholder="Email"
+        />
+      </div>
 
-    <div class="flex items-center justify-between">
-      <button
-        @click="submitRegistration"
-        class="btn-primary disabled:bg-slate-400"
-        :disabled="processingRegistration == true"
-        type="button"
-      >
-        Inscrever-se
-      </button>
+      <!-- Campo de senha -->
+      <div class="mb-6">
+        <label
+          class="block text-slate-600 text-sm font-bold mb-2"
+          for="password"
+        >
+          Senha
+        </label>
+        <input
+          class="shadow appearance-none border-2 rounded border-slate-600 w-full py-2 px-3 text-slate-600 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+          id="password"
+          type="password"
+          v-model="registrationForm.password"
+          placeholder="******************"
+        />
+      </div>
+
+      <div class="flex items-center justify-between">
+        <button
+          @click="submitRegistration"
+          class="btn-primary disabled:bg-slate-400"
+          :disabled="processingRegistration == true"
+          type="button"
+        >
+          Inscrever-se
+        </button>
+      </div>
     </div>
   </div>
 </template>
