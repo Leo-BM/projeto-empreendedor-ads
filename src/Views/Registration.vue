7a1865c0737ce64996e6df3f57ddbaf637d96348
Leo-BM <leo_bmaria@yahoo.com>
2023-09-30 12:04:41 -0300

    Reparado tela de Registro

--- src/Views/Registration.vue
+++ src/Views/Registration.vue
@@ -1,8 +1,8 @@
 <template>
-  <div class="bg-white p-8 rounded-lg shadow-md w-96">
-    <h1 class="text-2xl font-bold mb-4">Registre-se</h1>
-    <span class="text-black">{{ registrationForm }}</span>
-
+  <div class="bg-white p-8 rounded-lg shadow-md w-96 mx-auto">
+    <div class="text-center">
+      <MainLogo />
+    </div>
     <!-- Mensagem de erro geral -->
     <div
       class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative mb-4 invisible"
@@ -14,11 +14,14 @@
 
     <!-- Campo de nome completo -->
     <div class="mb-4">
-      <label class="block text-gray-700 text-sm font-bold mb-2" for="full_name">
+      <label
+        class="block text-slate-600 text-sm font-bold mb-2"
+        for="full_name"
+      >
         Nome completo
       </label>
       <input
-        class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+        class="shadow appearance-none border-2 rounded border-slate-600 w-full py-2 px-3 text-slate-600 leading-tight focus:outline-none focus:shadow-outline"
         id="full_name"
         v-model="registrationForm.full_name"
         type="text"
@@ -29,13 +32,13 @@
     <!-- Campo de data de nascimento -->
     <div class="mb-4">
       <label
-        class="block text-gray-700 text-sm font-bold mb-2"
+        class="block text-slate-600 text-sm font-bold mb-2"
         for="birth_date"
       >
         Data de nascimento
       </label>
       <input
-        class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+        class="shadow appearance-none border-2 rounded border-slate-600 w-full py-2 px-3 text-slate-600 leading-tight focus:outline-none focus:shadow-outline"
         id="birth_date"
         v-model="registrationForm.birth_date"
         type="date"
@@ -45,11 +48,11 @@
 
     <!-- Campo de e-mail -->
     <div class="mb-4">
-      <label class="block text-gray-700 text-sm font-bold mb-2" for="email">
+      <label class="block text-slate-600 text-sm font-bold mb-2" for="email">
         Email
       </label>
       <input
-        class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+        class="shadow appearance-none border-2 rounded border-slate-600 w-full py-2 px-3 text-slate-600 leading-tight focus:outline-none focus:shadow-outline"
         id="email"
         v-model="registrationForm.email"
         type="email"
@@ -59,30 +62,26 @@
 
     <!-- Campo de senha -->
     <div class="mb-6">
-      <label class="block text-gray-700 text-sm font-bold mb-2" for="password">
+      <label class="block text-slate-600 text-sm font-bold mb-2" for="password">
         Senha
       </label>
       <input
-        class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+        class="shadow appearance-none border-2 rounded border-slate-600 w-full py-2 px-3 text-slate-600 mb-3 leading-tight focus:outline-none focus:shadow-outline"
         id="password"
         type="password"
         v-model="registrationForm.password"
         placeholder="******************"
       />
-      <!-- Mensagem de erro para senha -->
-      <p class="text-red-500 text-xs italic hidden" id="passwordError">
-        Sua senha está incorreta.
-      </p>
     </div>
 
     <div class="flex items-center justify-between">
       <button
         @click="submitRegistration"
-        class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline disabled:bg-slate-400"
+        class="btn-primary disabled:bg-slate-400"
         :disabled="processingRegistration == true"
         type="button"
       >
-        Entrar
+        Inscrever-se
       </button>
     </div>
   </div>
@@ -92,6 +91,8 @@
 import { ref } from "vue";
 import axios from "axios";
 import { useRouter } from "vue-router";
+
+import MainLogo from "../Components/MainLogo.vue";
 
 const registrationFailMessage = ref(".");
 const processingRegistration = ref(false);
@@ -116,7 +117,7 @@ function submitRegistration() {
       },
     })
     .then((response) => {
-      router.push({ name: "DashboardPage" });
+      router.push({ name: "homePage" });
     })
     .catch((error) => {
       registrationFailMessage.value = error.response.data.mensagem;
@@ -130,4 +131,8 @@ function submitRegistration() {
 }
 </script>
 
-<style scoped></style>
+<style scoped>
+.acessos {
+  @apply inline-block w-32 text-center;
+}
+</style>
